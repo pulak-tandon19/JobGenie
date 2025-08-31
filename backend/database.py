9fe1a1e5e3add42aa5203c692bcce805bf96739d
pulak-tandon19 <pulaktandon2000@gmail.com>
2025-09-01 00:11:36 +0530

    basic docker setup

---
+++ backend/database.py
@@ -0,0 +1,9 @@
+# from sqalchemy import create_engine
+# from sqalchemy.ext.declarative import declarative_base
+# from sqalchemy.orm import sessionmaker
+
+# DATABASE_URL = "postgresql+psycopg2://postgres:password@db:5432/example_db"
+
+# engine = create_engine(DATABASE_URL)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
