3270867575b35e81c245de1bbfd6bfdc6396f01c
pulak-tandon19 <pulaktandon2000@gmail.com>
2025-10-03 02:24:58 +0530

    cache code correction

--- frontend/components/auth/services.py
+++ frontend/components/auth/services.py
@@ -1,16 +1,8 @@
 import streamlit as st
 import requests
-from streamlit_cookies_manager import EncryptedCookieManager
 
-from config import BASE_API_URL
+from config import BASE_API_URL, cookies
 
-cookies = EncryptedCookieManager(
-    prefix="jobgenie/",
-    password="super-secret-password"  # keep this safe!
-)
-
-if not cookies.ready():
-    st.stop()
 
 class AuthService:
     def register_user(self, email, password):
@@ -29,6 +21,7 @@ class AuthService:
         payload = {"email": email, "password": password}
         response = requests.post(f"{BASE_API_URL}/auth/login", json=payload)
         if response.status_code == 200:
+            print("reached if")
             data = response.json()
             access_token = data.get("access_token")
             refresh_token = data.get("refresh_token")
--- frontend/config.py
+++ frontend/config.py
@@ -1,6 +1,17 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
+from streamlit_cookies_manager import EncryptedCookieManager
+
 
 load_dotenv()
 BASE_API_URL = os.getenv("BASE_API_URL", "http://localhost:8000")
-# BASE_API_URL = "http://127.0.0.1:8000"
+
+cookies = EncryptedCookieManager(
+    prefix="jobgenie/",
+    password="super-secret-password"  # keep this safe!
+)
+
+if not cookies.ready():
+    print("reached cookie if")
+    st.stop()
